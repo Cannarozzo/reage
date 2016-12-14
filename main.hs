d5a8c9db346673491e4e645dde3810f7c4d55cda
babibonna <babibonna@hotmail.com>
2016-12-14 00:11:56 +0000

    implementando rotas

--- main.hs
+++ main.hs
@@ -7,6 +7,7 @@ import Data.Text
 import Data.Time.Calendar
 import Data.Time.Clock
 
+
 import PersistentFields
 
 
@@ -26,27 +27,29 @@ Usuario json
     pais        Text
     avatar      Int
     cor         Int
+    UniqueUsuario email
 Publicacao json
+    usuarioId       UsuarioId
     conteudo        Text
     dataCompleta    UTCTime 
-    categoria       Bool
+    categoria       CategoriaTipo
 Categoria json
-    categoria  CategoriaTipo
+    categoria   CategoriaTipo
     descricao   Text
-
-    
 |]
 
 mkYesod "Reage" [parseRoutes|
 / HomeR GET
-/usuario/obter/#UsuarioId UsuarioGetR GET
-/usuario/inserir UsuarioInserirR POST OPTIONS -- CADASTRO DO USUARIO, ALTERAÇÃO E DELETE,
-/usuario/editar/#UsuarioId UsuarioConfR PUT DELETE -- ALTERAÇÃO E DELETE
+/usuario/login                   UsuarioLoginR      POST OPTIONS
+/usuario/obter/#UsuarioId        UsuarioGetR        GET OPTIONS
+/usuario/inserir                 UsuarioInserirR    POST OPTIONS -- CADASTRO DO USUARIO, ALTERAÇÃO E DELETE,
+/usuario/editar/#UsuarioId       UsuarioConfR       PUT -- ALTERAÇÃO E DELETE
+/usuario/remover/#UsuarioId      UsuarioRemoverR    DELETE OPTIONS
+/usuario/publicar                UsuarioPublicarR   POST OPTIONS  -- FAZER A PUBLICAÇÃO 
 
-/publicar PublicarR  OPTIONS POST -- FAZER A PUBLICAÇÃO 
-/feed FeedR GET -- MOSTRAR PUBLICAÇÃO NO FEED POR DATA
--- perfil PerfilR GET -- MOSTRAR PERFIL DO USUARIO E PUBLICAÇÕES FEITAS POR ELE
-/buscar/#Text BuscaR GET
+/publicacoes                     PublicacoesR       GET OPTIONS -- MOSTRAR PUBLICAÇÃO NO FEED POR DATA
+    -- perfil PerfilR GET -- MOSTRAR PERFIL DO USUARIO E PUBLICAÇÕES FEITAS POR ELE
+/buscar/#Text                    BuscarR             GET OPTIONS
 
 
 
@@ -69,29 +72,56 @@ getHomeR = selectRep $ do
                provideJson $ object ["saudacao" .= ("ola json" :: Text)]
 --http://www.yesodweb.com/book/restful-contentstac
 
+-- == usuario/login UsuarioLoginR
+optionsUsuarioLoginR :: Handler ()
+optionsUsuarioLoginR = do 
+    addHeader "Access-Control-Allow-Origin" "*"
+    addHeader "Access-Control-Allow-Headers" "Origin, X-Requested-With, Content-Type, Accept"
+    addHeader "Access-Control-Allow-Methods" "POST"
 
+postUsuarioLoginR :: Handler Value
+postUsuarioLoginR = do
+    addHeader "Access-Control-Allow-Origin" "*"
+    usuario <- requireJsonBody :: Handler Usuario
+    usuarioTalvez <- runDB $ selectFirst [UsuarioEmail ==. (usuarioEmail usuario), UsuarioSenha ==. (usuarioSenha usuario)] []
+    case usuarioTalvez of 
+        Nothing -> 
+            returnJson $ object ["resp" .= object ["usuario" .= pack "-1"] ]
+        Just (Entity uid usuario) ->
+            returnJson $ object ["resp" .= object ["usuario" .= uidText ] ]
+            where 
+                uidText = (pack.show.fromSqlKey) uid 
+              
+-- == usuario/obter UsuarioGetR
+optionsUsuarioGetR :: UsuarioId -> Handler ()
+optionsUsuarioGetR uid = do 
+    addHeader "Access-Control-Allow-Origin" "*"
+    addHeader "Access-Control-Allow-Headers" "Origin, X-Requested-With, Content-Type, Accept"
+    addHeader "Access-Control-Allow-Methods" "POST"
 
--- == UsuarioInserirR 
 getUsuarioGetR :: UsuarioId -> Handler ()
 getUsuarioGetR uid = do
+    addHeader "Access-Control-Allow-Origin" "*" 
     usuario <- runDB $ get404 uid
-    sendResponse $ toJSON usuario
+    sendResponse $ toJSON usuario 
     
+-- == usuario/inserir UsuarioInserirR     
 optionsUsuarioInserirR :: Handler ()
 optionsUsuarioInserirR = do 
     addHeader "Access-Control-Allow-Origin" "*"
     addHeader "Access-Control-Allow-Headers" "Origin, X-Requested-With, Content-Type, Accept"
     addHeader "Access-Control-Allow-Methods" "POST"
 
-
 postUsuarioInserirR :: Handler Value
 postUsuarioInserirR = do
     addHeader "Access-Control-Allow-Origin" "*"
     usuario <- requireJsonBody :: Handler Usuario
-    runDB $ insert usuario
-    returnJson $ (object [pack "resp" .= pack "CREATED"])
+    usuarioid <- runDB $ insert usuario
+    returnJson $ object [pack "resp" .= object [pack "CREATED" .= (pack $ show $ fromSqlKey usuarioid)] ] -- 
 
+--  returnJson $ (object [pack "resp" .= pack "CREATED", pack "usuarioId" .= (pack $ show $ fromSqlKey usuarioid)])
 
+-- == usuario/editar UsuarioConfR 
 putUsuarioConfR :: UsuarioId -> Handler ()
 putUsuarioConfR cid = do
     addHeader "Access-Control-Allow-Origin" "*"
@@ -104,36 +134,52 @@ putUsuarioConfR cid = do
     sendResponse (object [pack "resp" .= pack "Changed"])
     
 
-deleteUsuarioConfR :: UsuarioId -> Handler ()
-deleteUsuarioConfR cid = do
+-- == usuario/remover
+optionsUsuarioRemoverR :: UsuarioId -> Handler ()
+optionsUsuarioRemoverR uid = do 
+    addHeader "Access-Control-Allow-Origin" "*"
+    addHeader "Access-Control-Allow-Headers" "Origin, X-Requested-With, Content-Type, Accept"
+    addHeader "Access-Control-Allow-Methods" "DELETE"
+
+deleteUsuarioRemoverR :: UsuarioId -> Handler ()
+deleteUsuarioRemoverR cid = do
     addHeader "Access-Control-Allow-Origin" "*"
     runDB $ delete cid
     sendResponse (object [pack "resp".= pack "Deleted"])
 
-optionsPublicarR :: Handler ()
-optionsPublicarR = do 
+-- == usuario/postar UsuarioPostarR
+optionsUsuarioPublicarR :: Handler ()
+optionsUsuarioPublicarR = do 
     addHeader "Access-Control-Allow-Origin" "*"
     addHeader "Access-Control-Allow-Headers" "Origin, X-Requested-With, Content-Type, Accept"
     addHeader "Access-Control-Allow-Methods" "POST"
     
-postPublicarR :: Handler ()
-postPublicarR = do
+postUsuarioPublicarR :: Handler ()
+postUsuarioPublicarR = do
     addHeader "Access-Control-Allow-Origin" "*"
     publicacao <- requireJsonBody :: Handler Publicacao
     runDB $ insert publicacao
     sendResponse (object [pack "resp" .= pack "CREATED"])
 
     
-getFeedR :: Handler ()
-getFeedR = do
+-- == publicacoes   PublicacoesR   
+
+optionsPublicacoesR :: Handler ()
+optionsPublicacoesR = undefined
+
+getPublicacoesR :: Handler ()
+getPublicacoesR = do
     addHeader "Access-Control-Allow-Origin" "*"
     publicacao <- runDB $ selectList [] [Asc PublicacaoDataCompleta]
     sendResponse (object["usuario" .= fmap toJSON publicacao])
 
     
+-- == buscar
+optionsBuscarR ::Text -> Handler ()
+optionsBuscarR texto = undefined
     
-getBuscaR :: Text -> Handler TypedContent
-getBuscaR str = undefined
+getBuscarR :: Text -> Handler TypedContent
+getBuscarR str = undefined
 --    var <- runDB $ selectList [CategoriaTipo ==.str] [Asc PublicacaoDataReg]
 --      sendResponse (object["usuario" .= fmap toJSON publicacao])
 
@@ -144,7 +190,7 @@ connectionInfo = defaultConnectInfo { connectHost = "localhost",
                                       connectPort = 3306,
                                       connectUser = "babibonna",
                                       connectPassword = "",
-                                      connectDatabase = "reagedb1"}
+                                      connectDatabase = "reagedb"}
 -- connStr = "dbname=dd9en8l5q4hh2a host=ec2-107-21-219-201.compute-1.amazonaws.com user=kpuwtbqndoeyqb password=aCROh525uugAWF1l7kahlNN3E0 port=5432"
 -- https://hackage.haskell.org/package/persistent-mysql-2.1/docs/Database-Persist-MySQL.html#t:ConnectInfo
 main::IO()
